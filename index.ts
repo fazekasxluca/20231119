9bb88c1124853f04a6d8e0a81ead1da1306b779c
luca <fazekasxluca98@gmail.com>
2023-11-24 09:31:45 +0100

    TS hozzaAdva

--- index.ts
+++ index.ts
@@ -28,8 +28,9 @@ function keruletTerulet(sugar: number) : [number,number]{
     return tuple;
 }
 
-
+var asd;
 var eredmeny1 = console.log(keruletTerulet(4));
+
 
 interface Auto
 {
@@ -38,3 +39,46 @@ interface Auto
     hengurtart:number,
     benzinesE:boolean
 }
+
+
+var auto1: Auto ={
+
+   gyarto:"Opel", tipus:"204",hengurtart:1200,benzinesE:true,   
+}
+var auto2: Auto ={
+
+  gyarto:"Peudgeot", tipus:"D",hengurtart:900,benzinesE:true,   
+}
+var auto3: Auto ={
+
+  gyarto:"Mazda", tipus:"N",hengurtart:1300,benzinesE:true,   
+}
+var auto4: Auto ={
+
+  gyarto:"Ford", tipus:"M",hengurtart:1600,benzinesE:true,   
+}
+
+var autokTomb:Auto[] = [auto1,auto2,auto3,auto4];
+
+
+function minimumKeres(tomb:Auto[]): Auto
+{
+
+  var henrUrtartMin:Auto = tomb[0];
+
+  for(var i:number = 0; i < tomb.length;i++)
+  {
+      if(henrUrtartMin.hengurtart > tomb[i].hengurtart)
+      {
+        henrUrtartMin.hengurtart = tomb[i].hengurtart;
+      }
+  }
+
+  return henrUrtartMin;
+
+}
+
+
+var eredmeny2 = console.log(minimumKeres(autokTomb));
+
+
